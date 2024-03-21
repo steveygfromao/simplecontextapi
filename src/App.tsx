070ecccb3b76e655b4049c7c2498aad8f04f1638
Steve Green <121233991+steveygfromao@users.noreply.github.com>
2024-03-21 13:46:59 +0000

    Update App.tsx
    Removed unwanted code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
-
 import Login from './components/Login';
 import Profile from './components/Profile';
 import UserContextProvider from './context/UserContextProvider';
-
-const queryClient = new QueryClient()
 
 function App() {
   return (
